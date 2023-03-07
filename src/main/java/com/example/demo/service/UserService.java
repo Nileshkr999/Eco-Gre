d759e81448c2ff622617739064c8a022c758b785
Nilesh Singh <nilesh.singh@girmiti.in>
2023-03-07 19:54:36 +0530

    created project
    description

---
+++ src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,11 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import com.example.demo.model.User;
+
+public interface UserService {
+
+	 public void saveUser(User user);
+	    public List<Object> isUserPresent(User user);
+}
